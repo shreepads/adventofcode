d4f67b5c4107649c7eb79f526b2c911920498517
Shreepad S <shreepads@gmail.com>
2021-12-11 15:53:26 +0530

    Solve Day 1 #2

--- 2021/y2021/src/main.rs
+++ 2021/y2021/src/main.rs
@@ -1,8 +1,12 @@
 use day1_1;
+use day1_2;
 
 fn main() {
     println!("Hello, world!");
     println!("Day 1, #1:{}",
         day1_1::count_depth_inc(&String::from("resources/day1-1-input.txt"))
+    );
+    println!("Day 1, #2:{}",
+        day1_2::count_depth_inc_slider(&String::from("resources/day1-1-input.txt"))
     );
 }
