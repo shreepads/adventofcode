da7b919f11df3425c569d482ef0cc1f2d205e67a
Shreepad S <shreepads@gmail.com>
2021-12-12 13:05:16 +0530

    Setup core functions for Day 3 #2

--- 2021/day3_2/src/lib.rs
+++ 2021/day3_2/src/lib.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::collections::HashSet;
 
 pub fn calculate_oxygen_co2(file_path: &String) -> (i64, i64) {
 
@@ -7,13 +8,55 @@ pub fn calculate_oxygen_co2(file_path: &String) -> (i64, i64) {
     let contents = fs::read_to_string(file_path)
         .expect("Something went wrong reading the file");
 
-    let  lines = contents.lines();
+    let mut report_data: HashSet<Vec<u32>> = HashSet::new();
 
-    println!("Lines count: {}", lines.count());
+    init_report_data(&mut report_data, contents);
 
-    (3, 4)
+    // Need a copy to run both checks
+    let mut report_data_2 = report_data.clone();
+
+    println!("Data row count: {}", report_data.len());
+
+    println!("Data 2 row count: {}", report_data_2.len());
+
+    let oxygen = calculate_metric(&mut report_data, true);
+
+    let co2 = calculate_metric(&mut report_data_2, false);
+
+    (oxygen, co2)
 }
 
+fn init_report_data(report_data: &mut HashSet<Vec<u32>>, contents: String ) {
+
+    let lines = contents.lines();
+
+    for line in lines {
+
+        let mut row: Vec<u32> = Vec::new();
+
+        for charac in line.chars() {
+            match charac.to_digit(2) {
+                Some(x) => row.push(x),
+                None    => println!("Non binary digit in {}", line),
+            }
+        };
+    
+        if !report_data.insert(row) {
+            println!("Duplicate record!!");
+        }
+
+    }
+
+}
+
+fn calculate_metric(report_data: &mut HashSet<Vec<u32>>, most_common: bool) -> i64{
+    if most_common {
+        return 12;
+    }
+    else {
+        return 3;
+    }
+}
 
 
 #[cfg(test)]
