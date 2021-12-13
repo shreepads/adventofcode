d03a2a2ee2c6d2d5dd9394b9406aa4fac4d16c7e
Shreepad S <shreepads@gmail.com>
2021-12-13 23:40:03 +0530

    Solve Day 5 #1

--- 2021/day5_1/src/grid.rs
+++ 2021/day5_1/src/grid.rs
@@ -1,17 +1,67 @@
 // Copyright (c) 2021 Shreepad Shukla
 // SPDX-License-Identifier: MIT
 
+
 #[derive(Debug, Copy, Clone)]
 pub struct Grid {
-    grid_data: [[u32; 1000]; 1000],
+    grid_data: [[u8; 1000]; 1000],  // u32 causes a stack overflow
 }
 
 impl Grid {
 
     pub fn new() -> Grid {
         Grid {
-            grid_data: [[0u32; 1000]; 1000],
+            grid_data: [[0u8; 1000]; 1000],
         }
+    }
+
+    pub fn add_line(&mut self, x1: usize, y1: usize, x2: usize, y2: usize, add_diagonals: bool) {
+
+        // Exclude diagonals if flagged
+        if x1 != x2  &&  y1 != y2 && !add_diagonals {
+            return
+        }
+
+        // Add vertical line
+        if x1 == x2 {
+            let mut start = y1;
+            let mut end = y2;
+            if y1 > y2 {
+                start = y2;
+                end = y1;
+            }
+            for y in start..=end {
+                self.grid_data[y][x1] += 1;
+            }
+            return;
+        }
+
+        // Add horizontal line
+        if y1 == y2 {
+            let mut start = x1;
+            let mut end = x2;
+            if x1 > x2 {
+                start = x2;
+                end = x1;
+            }
+            for x in start..=end {
+                self.grid_data[y1][x] += 1;
+            }
+            return;
+        }
+    }
+
+    pub fn overlap_points(self) -> u32 {
+        
+        let mut overlap_points = 0u32;
+
+        for row in self.grid_data.iter() {
+            overlap_points += row.iter()
+                .filter(|x| **x > 1)
+                .fold(0, |acc, _| acc + 1);   // using count() causes type mismatch
+        }
+
+        overlap_points
     }
 
 }
--- 2021/day5_1/src/lib.rs
+++ 2021/day5_1/src/lib.rs
@@ -17,10 +17,35 @@ pub fn calculate_overlap_points(file_path: &String) -> u32 {
 
     load_lines(&mut grid, contents);
 
-    0u32
+    grid.overlap_points()
 }
 
 fn load_lines(grid: &mut Grid, contents: String) {
+
+    for line in contents.lines() {
+
+        // println!("Adding line: {}", line);
+        
+        let mut x1 = 0usize;
+        let mut y1 = 0usize;
+        let mut x2 = 0usize;
+        let mut y2 = 0usize;
+
+        for (i, coords) in line.split(" -> ").enumerate() {
+            if i == 0 {
+                let mut coord = coords.split(",");
+                x1 = coord.next().unwrap().parse::<usize>().unwrap();
+                y1 = coord.next().unwrap().parse::<usize>().unwrap();
+            }
+            else {
+                let mut coord = coords.split(",");
+                x2 = coord.next().unwrap().parse::<usize>().unwrap();
+                y2 = coord.next().unwrap().parse::<usize>().unwrap();
+            }
+        }
+
+        grid.add_line(x1, y1, x2, y2, false);
+    }
 
 }
 
