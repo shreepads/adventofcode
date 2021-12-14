d229b7cc2ed5ba151de2683f0d0d8e5574ed7884
Shreepad S <shreepads@gmail.com>
2021-12-15 00:49:25 +0530

    Solve Day 7 #2

--- 2021/day7_2/src/lib.rs
+++ 2021/day7_2/src/lib.rs
@@ -16,11 +16,36 @@ pub fn calculate_least_fuel(file_path: &String) -> u32 {
         crab_posns.push(crab_posn.parse::<u32>().unwrap());
     }
 
-    let min_fuel = u32::MAX;
+    let mut min_fuel = u32::MAX;
 
+    let min_posn = *crab_posns.iter().min().unwrap();  // Need to deref the unwrap
+    let max_posn = *crab_posns.iter().max().unwrap();
 
+    for posn in min_posn..=max_posn {
+        let fuel = calculate_fuel(&crab_posns, posn);
+        if fuel < min_fuel {
+            min_fuel = fuel;
+        }
+    }
 
     min_fuel
+}
+
+fn calculate_fuel(crab_posns: &Vec<u32>, input_posn: u32) -> u32 {
+
+    let mut fuel = 0u32;
+
+    for posn in crab_posns.iter() {
+        if posn < &input_posn {
+            fuel += ((input_posn - posn) * (input_posn - posn + 1))/2
+        }
+        else {
+            fuel += ((posn - input_posn) * (posn - input_posn + 1))/2
+        }
+    }
+
+    fuel
+
 }
 
 
