a0cd7e57604bb7aaf47ab7f60ac894f2ca0e5604
Shreepad S <shreepads@gmail.com>
2021-12-24 18:51:51 +0530

    Complete explode of snailfish number

--- 2021/day18_1/src/sfish_math.rs
+++ 2021/day18_1/src/sfish_math.rs
@@ -192,7 +192,7 @@ impl Number {
         }
 
         // create new parent node
-        let mut new_rootnode = Node {
+        let new_rootnode = Node {
             part1_id: Some(self.rootnode_id),
             part2_id: Some(sfish_no.rootnode_id + id_delta),
             parent_id: None,
@@ -207,7 +207,239 @@ impl Number {
         self.rootnode_id = self.nodes.len() - 1;
 
     }
+
+    pub fn reduce(&mut self) {
+        
+        let mut done = false;
+
+        while !done {
+            // find leftmost node more than 5 deep
+            if let Some(explode_id) = self.leftmost_explode_id() {
+                // explode it
+                let mut sfish_string = String::new();
+                self.stringify(&mut sfish_string, explode_id);
+                println!("Exploding node {}: {}", explode_id, sfish_string);
+
+                // add part1 value to first regular number on left
+                self.add_to_leftid(explode_id);
+                self.add_to_rightid(explode_id);
+
+                // replace with 0
+                self.replace_with_zero(explode_id);
+
+                done = true;    // remove once done
+            } else {
+                // no more to explode
+                done = true;
+            }
+
+            
+        }
+
+    }
+
+    fn leftmost_explode_id(&self) -> Option<usize> {
+        
+        for (i, node) in self.nodes.iter().enumerate() {
+            
+            println!("Checking node {}", i);
+            
+            // check depth
+            if node.depth < 4 {
+                continue;
+            }
+            
+            // check if it is a leaf node
+            if !node.pair {
+                continue;
+            }
+
+            // check if parts are leaf nodes
+            if let Some(part1) = node.part1_id {
+                if let Some(part2) = node.part2_id {
+                    if !self.nodes[part1].pair  &&  !self.nodes[part2].pair {
+                        return Some(i);
+                    }
+                } else {
+                    println!("No part2 for node {} with pair", i);
+                }
+            } else {
+                println!("No part1 for node {} with pair", i);
+            }
+        }
+
+        None
+    }
+
+    fn add_to_leftid(&mut self, explode_id: usize) {
+
+        let part1_id = self.nodes[explode_id].part1_id.unwrap();
+        let part1_value = self.nodes[part1_id].value.unwrap();
+
+        let mut parent_id = self.nodes[explode_id].parent_id.unwrap();
+        let mut child_id = explode_id;
+        let mut left_id = explode_id;
+
+        let mut done = false;
+
+        // traverse up till find parent whose part2 is in chain
+        while !done {
+            
+            let parent_node = self.nodes[parent_id];
+            let child_node = self.nodes[child_id];
+
+            if parent_node.part1_id == Some(child_id) {
+                // need to go further up, unless reached root
+                if parent_id == self.rootnode_id {
+                    // there is no left node
+                    return;
+                }
+
+                child_id = parent_id;
+                parent_id = parent_node.parent_id.unwrap();
+            } else {
+                // found node to descend
+                done = true;
+            }
+        }
+
+        // switch to part1 chain and traverse down till last part2
+        child_id = self.nodes[parent_id].part1_id.unwrap();
+        done = false;
+
+        while !done {
+            
+            let child_node = self.nodes[child_id];
+
+            // if child_id is a leaf node we're done
+            if !child_node.pair {
+                left_id = child_id;
+                done = true;
+                continue;
+            }
+
+            // traverse down part2
+            parent_id = child_id;
+            child_id = child_node.part2_id.unwrap();
+        }
+
+        // did we find the left Id
+        if left_id != explode_id {
+            // increment value
+
+            let mut sfish_string = String::new();
+            self.stringify(&mut sfish_string, left_id);
+            println!("Adding {} to left node {}: {}", part1_value, left_id, sfish_string);
+
+            if let Some(mut value) = self.nodes[left_id].value {
+                self.nodes[left_id].value = Some(value + part1_value);
+            }
+        }
+
+        return;
+    }
+
+
+    fn add_to_rightid(&mut self, explode_id: usize) {
+
+        let part2_id = self.nodes[explode_id].part2_id.unwrap();
+        let part2_value = self.nodes[part2_id].value.unwrap();
+
+        let mut parent_id = self.nodes[explode_id].parent_id.unwrap();
+        let mut child_id = explode_id;
+        let mut right_id = explode_id;
+
+        let mut done = false;
+
+        // traverse up till find parent whose part1 is in chain
+        while !done {
+            
+            let parent_node = self.nodes[parent_id];
+            let child_node = self.nodes[child_id];
+
+            if parent_node.part2_id == Some(child_id) {
+                // need to go further up, unless reached root
+                if parent_id == self.rootnode_id {
+                    // there is no right node
+                    return;
+                }
+
+                child_id = parent_id;
+                parent_id = parent_node.parent_id.unwrap();
+            } else {
+                // found node to descend
+                done = true;
+            }
+        }
+
+        // switch to part2 chain and traverse down till last part1
+        child_id = self.nodes[parent_id].part2_id.unwrap();
+        done = false;
+
+        while !done {
+            
+            let child_node = self.nodes[child_id];
+
+            // if child_id is a leaf node we're done
+            if !child_node.pair {
+                right_id = child_id;
+                done = true;
+                continue;
+            }
+
+            // traverse down part1
+            parent_id = child_id;
+            child_id = child_node.part1_id.unwrap();
+        }
+
+        // did we find the right Id
+        if right_id != explode_id {
+            // increment value
+
+            let mut sfish_string = String::new();
+            self.stringify(&mut sfish_string, right_id);
+            println!("Adding {} to right node {}: {}", part2_value, right_id, sfish_string);
+
+            if let Some(mut value) = self.nodes[right_id].value {
+                self.nodes[right_id].value = Some(value + part2_value);
+            }
+        }
+
+        return;
+    }
+
     
+    fn replace_with_zero(&mut self, explode_id: usize) {
+
+        let depth = self.nodes[explode_id].depth;
+        let parent_id = self.nodes[explode_id].parent_id.unwrap();
+        let mut parent_node = self.nodes[parent_id];
+
+        let zero_node = Node {
+            part1_id: None,
+            part2_id: None,
+            parent_id: Some(parent_id),
+            value: Some(0),
+            left_id: None,
+            right_id: None,
+            pair: false,
+            depth: depth,
+        };
+
+        self.nodes.push(zero_node);
+        
+        // Set parent's child part to explode id to zero
+        if parent_node.part1_id == Some(explode_id) {
+            self.nodes[parent_id].part1_id = Some(self.nodes.len() - 1);
+        } else {
+            self.nodes[parent_id].part2_id = Some(self.nodes.len() - 1);
+        }
+
+        return
+
+    }
+
+
     pub fn magnitude(&self) -> u32 {
         0
     }
@@ -270,22 +502,67 @@ mod tests {
 
     
     #[test]
-    fn new_sno_two_pairs() {
-        let input = "[[1,9],[8,5]]";
+    fn new_sno_complex() {
+        let input = "[[[2,3],[[4,5],6]],[9,[8,7]]]";
         let result = Number::new(input.to_string());
         println!("Sfish Number: {:#?}", result);
         assert_eq!(input, result.to_string());
     }
 
     #[test]
-    fn add() {
-        let mut sno1 = Number::new("[2,3]".to_string());
-        let sno2 = Number::new("[4,5]".to_string());
+    fn add_1() {
+        let mut sno1 = Number::new("[1,2]".to_string());
+        let sno2 = Number::new("[[3,4],5]".to_string());
         sno1.add(&sno2);
         println!("Addition: {:#?}", sno1);
-        println!("Addition str: {}", sno1.to_string());
-        assert_eq!(2, 3);  // invalid to force print
+        assert_eq!("[[1,2],[[3,4],5]]", sno1.to_string());
+    }
+
+    #[test]
+    fn add_2() {
+        let mut sno1 = Number::new("[2,3]".to_string());
+        let sno2 = Number::new("[[4,5],6]".to_string());
+        let sno3 = Number::new("[9,[8,7]]".to_string());
+        sno1.add(&sno2);
+        sno1.add(&sno3);
+        println!("Addition: {:#?}", sno1);
+        assert_eq!("[[[2,3],[[4,5],6]],[9,[8,7]]]", sno1.to_string());
     }
 
 
+    #[test]
+    fn explode_none_simple() {
+        let input = "[2,3]";
+        let mut sno = Number::new(input.to_string());
+        sno.reduce();
+        println!("Reduced: {}", sno);
+        assert_eq!(input, sno.to_string());
+    }
+
+    #[test]
+    fn explode_none_complex() {
+        let input = "[[[2,3],[[4,5],6]],[9,[8,7]]]";
+        let mut sno = Number::new(input.to_string());
+        sno.reduce();
+        println!("Reduced: {}", sno);
+        assert_eq!(input, sno.to_string());
+    }
+
+
+    #[test]
+    fn single_explode_rt() {
+        let mut sno = Number::new("[[[[[9,8],1],2],3],4]".to_string());
+        sno.reduce();
+        println!("Reduced: {}", sno);
+        assert_eq!("[[[[0,9],2],3],4]", sno.to_string());  // force print
+    }
+
+
+    #[test]
+    fn single_explode_lt() {
+        let mut sno = Number::new("[7,[6,[5,[4,[3,2]]]]]".to_string());
+        sno.reduce();
+        println!("Exploded: {}", sno);
+        assert_eq!("[7,[6,[5,[7,0]]]]", sno.to_string());    // force print
+    }
 }
