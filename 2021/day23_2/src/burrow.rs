dcfa1e6f823e7721c20c1550014a76dbce84ac19
Shreepad S <shreepads@gmail.com>
2021-12-29 19:14:56 +0530

    Setup burrow mod for Day 23 #2

---
+++ 2021/day23_2/src/burrow.rs
@@ -0,0 +1,114 @@
+// Copyright (c) 2021 Shreepad Shukla
+// SPDX-License-Identifier: MIT
+
+use std::fmt;
+use std::collections::HashMap;
+
+#[derive(Debug, Clone, PartialEq, Copy, Eq, Hash)]
+pub enum AmphiType {
+    A,
+    B,
+    C,
+    D,
+}
+
+impl AmphiType {
+    pub fn energy(&self) -> u32 {   // because Rust can't do static HashMaps
+
+        use self::AmphiType::*;
+
+        match self {
+            A => 1,
+            B => 10,
+            C => 100,
+            D => 1000,
+        }
+    }
+
+    pub fn to_char(&self) -> char {
+
+        use self::AmphiType::*;
+
+        match self {
+            A => 'A',
+            B => 'B',
+            C => 'C',
+            D => 'D',
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Copy, Eq, Hash)]
+pub enum PositionState {
+    Empty,
+    Occupied(AmphiType),
+}
+
+impl PositionState {
+    pub fn to_char(&self) -> char {
+        use self::PositionState::*;
+
+        match self {
+            Empty => '☐',
+            Occupied(at) => at.to_char(),
+        }
+
+    }
+}
+
+pub const NOSTOP_POS: [usize; 4] = [2, 4, 6, 8];
+pub const AHOME_POS: [usize; 4] = [11, 12, 13, 14];
+pub const BHOME_POS: [usize; 4] = [15, 16, 17, 18];
+pub const CHOME_POS: [usize; 4] = [19, 20, 21, 22];
+pub const DHOME_POS: [usize; 4] = [23, 24, 25, 26];
+
+
+#[derive(Debug, Clone, PartialEq, Copy, Eq, Hash)]
+pub struct BurrowState {
+    pub positions: [PositionState; 27],
+}
+
+impl BurrowState {
+
+    pub fn new_end() -> BurrowState {  
+        
+        use self::AmphiType::*;
+        use self::PositionState::*;
+
+        let mut posns: [PositionState; 27] = [Empty ; 27];
+
+        for pos in AHOME_POS { posns[pos] = Occupied(A); }
+        for pos in BHOME_POS { posns[pos] = Occupied(B); }
+        for pos in CHOME_POS { posns[pos] = Occupied(C); }
+        for pos in DHOME_POS { posns[pos] = Occupied(D); }
+
+        BurrowState {
+            positions: posns,
+        }
+    }
+
+
+    pub fn to_string(&self) -> String {
+        let mut str = String::new();
+        str.push('\n');
+        for i in 0..11 {str.push(self.positions[i].to_char());}
+        str.push('\n');
+        for row_no in 0..4 {
+            str.push_str(&format!("  {} {} {} {}\n",
+                self.positions[11 + row_no].to_char(),
+                self.positions[15 + row_no].to_char(),
+                self.positions[19 + row_no].to_char(),
+                self.positions[23 + row_no].to_char()
+            ));
+        }
+
+        str
+    }
+}
+
+impl fmt::Display for BurrowState {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.to_string())
+    }
+}
+
--- 2021/day23_2/src/lib.rs
+++ 2021/day23_2/src/lib.rs
@@ -1,9 +1,17 @@
 // Copyright (c) 2021 Shreepad Shukla
 // SPDX-License-Identifier: MIT
 
+mod burrow;
+
 use std::fs;
+use std::collections::HashMap;
+
+use burrow::AmphiType;
+use burrow::BurrowState;
+use burrow::PositionState;
 
 pub fn calculate_min_energy(file_path: &String) -> u32 {
+    
     println!("Loading data from file:{}", file_path);
 
     let contents = fs::read_to_string(file_path).expect(&format!(
@@ -11,7 +19,66 @@ pub fn calculate_min_energy(file_path: &String) -> u32 {
         file_path
     ));
 
+    let start_pos = load_data(contents);
+    let end_pos = BurrowState::new_end();
+
+    println!("Loaded start pos {}", start_pos);
+    println!("End pos {}", end_pos);
+
     0
+}
+
+
+fn load_data(contents: String) -> BurrowState {
+
+    use AmphiType::*;
+    use PositionState::*;
+
+    let mut posns: [PositionState; 27] = [Empty ; 27];
+
+    let mut lines = contents.lines();
+    lines.next();
+    lines.next();
+
+    for (row_no, line) in lines.enumerate() {
+
+        if line.contains("######") {
+            break;
+        }
+
+        let mut chars = line.chars();
+
+        let mut cols : [char; 4] = [' '; 4];
+        
+        cols[0] = chars.nth(3).unwrap();
+        cols[1] = chars.nth(1).unwrap();
+        cols[2] = chars.nth(1).unwrap();
+        cols[3] = chars.nth(1).unwrap();
+
+        for (col_no, col) in cols.iter().enumerate() {
+            
+            let posn_idx = 11 + row_no + (col_no * 4);
+            
+            posns[posn_idx] = match col {
+                'A' => Occupied(A),
+                'B' => Occupied(B),
+                'C' => Occupied(C),
+                'D' => Occupied(D),
+                _   => {
+                    println!("Invalid char {} at col no {} in line {}",
+                        col, col_no, line
+                    );
+                    Empty
+                },
+            }
+        }
+
+    }
+
+    BurrowState {
+        positions : posns,
+    }
+
 }
 
 
