d4b467ec566f32ee185a64b922eefb5cee43ee80
Shreepad S <shreepads@gmail.com>
2022-01-02 18:43:27 +0530

    Solve Day 25 #1

--- 2021/day25_1/src/lib.rs
+++ 2021/day25_1/src/lib.rs
@@ -2,7 +2,9 @@
 // SPDX-License-Identifier: MIT
 
 use std::fs;
+use std::{thread, time};
 
+#[derive(Debug, Clone, PartialEq, Copy, Eq, Hash)]
 pub enum Location {
     Empty,
     East,
@@ -22,9 +24,80 @@ pub fn calculate_stop_steps(file_path: &String) -> u32 {
 
     load_map(&mut map, contents);
 
-    print_map(&map);
+    let mut done = false;
+    let mut step = 1;
 
-    0
+    while !done {
+        done = seac_shuffle(&mut map);
+        step += 1;
+        //print_map(&map);
+        //thread::sleep(time::Duration::from_secs(3));
+    }
+   
+
+    step - 1
+}
+
+fn seac_shuffle(map: &mut Vec<Vec<Location>>) -> bool {
+
+    use crate::Location::*;
+
+    let mut map_clone = map.clone();
+    let mut shuffled = false;
+
+    let row_count = map.len();
+    let col_count = map[0].len();
+
+    // Move East
+    for (row_no, row) in map.iter().enumerate() {
+        for (col_no, loc) in row.iter().enumerate() {
+
+            let next_col_no = if col_no == (col_count - 1) {
+                0
+            } else {
+                col_no + 1
+            };
+
+            let next_loc = map[row_no][next_col_no];
+
+            if *loc == East  &&  next_loc == Empty {
+                map_clone[row_no][col_no] = Empty;
+                map_clone[row_no][next_col_no] = East;
+                shuffled = true;
+            }
+
+        }
+        
+    }
+
+    let mut map_clone2 = map_clone.clone();
+
+    // Move South
+    for (row_no, row) in map_clone.iter().enumerate() {
+        for (col_no, loc) in row.iter().enumerate() {
+
+            let next_row_no = if row_no == (row_count - 1) {
+                0
+            } else {
+                row_no + 1
+            };
+
+            let next_loc = map_clone[next_row_no][col_no];
+
+            if *loc == South  &&  next_loc == Empty {
+                map_clone2[row_no][col_no] = Empty;
+                map_clone2[next_row_no][col_no] = South;
+                shuffled = true;
+            }
+
+        }
+        
+    }
+
+    // Reassign clone to main
+    *map = map_clone2;
+
+    !shuffled
 }
 
 fn load_map(map: &mut Vec<Vec<Location>>, contents: String) {
@@ -52,17 +125,20 @@ fn print_map(map: &Vec<Vec<Location>>) {
 
     use crate::Location::*;
 
+    println!("Map:");
+
     for row in map.iter() {
         for loc in row.iter() {
             match loc {
                 Empty => print!("."),
                 South => print!("v"),
                 East  => print!(">"),
-                _     => print!("X"),
             }
         }
         println!("");
     }
+
+    println!("******");
 }
 
 
