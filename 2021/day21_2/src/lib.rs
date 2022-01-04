4487ac2910b368e431db8966c02ed1f3deeecef8
Shreepad S <shreepads@gmail.com>
2022-01-04 13:01:25 +0530

    Setup all core functions for Day 21 #2

--- 2021/day21_2/src/lib.rs
+++ 2021/day21_2/src/lib.rs
@@ -30,25 +30,71 @@ pub fn calculate_win_universe_count(p1_start_pos: usize,
 
     let mut round = 1;
     
-    while !player_won(&player1_states, &player2_states) {
+    loop {
         println!("Round: {}", round);
         println!("*********");
         print_states(&player1_states, &player2_states);
 
-        move_pawns(&mut player1_states, &mut player2_states, &throw_map);
+        move_pawns(&mut player1_states, &throw_map);
+
+        if won_all_universes(&player1_states, win_score) {
+            println!("**** PLAYER 1 WINS ****");
+            break;
+        }
+
+        move_pawns(&mut player2_states, &throw_map);
+
+        if won_all_universes(&player2_states, win_score) {
+            println!("**** PLAYER 2 WINS ****");
+            break;
+        }
+
         
         round += 1;
-        //thread::sleep(time::Duration::from_secs(3));
+        
     }
 
-    0
+    let player1_wins = count_win_universes(&player1_states, win_score);
+    let player2_wins = count_win_universes(&player2_states, win_score);
+
+    println!("Player 1 wins in {} univs, Player 2 in {}", player1_wins, player2_wins);
+    
+    if player1_wins > player2_wins {
+        player1_wins
+    } else {
+        player2_wins
+    }
 
 }
 
-fn player_won(player1_states: &[[u64; MAX_SCORE]; MAX_POS],
-    player2_states: &[[u64; MAX_SCORE]; MAX_POS]) -> bool {
+fn count_win_universes(player_states: &[[u64; MAX_SCORE]; MAX_POS], win_score: usize) -> u64 {
+    
+    let mut win_count = 0u64;
 
-    false
+    for (posn, posn_scores) in player_states.iter().enumerate() {
+        for (score, score_count) in posn_scores.iter().enumerate() {
+            if score < win_score {
+                continue;
+            }
+
+            win_count += score_count;
+        }
+    }
+
+    win_count
+}
+
+fn won_all_universes(player_states: &[[u64; MAX_SCORE]; MAX_POS], win_score: usize) -> bool {
+
+    for (posn, posn_scores) in player_states.iter().enumerate() {
+        for (score, score_count) in posn_scores.iter().enumerate() {
+            if score < win_score  &&  *score_count > 0 {
+                return false;
+            }
+        }
+    }
+
+    true
 
 }
 
@@ -79,17 +125,17 @@ fn print_states(player1_states: &[[u64; MAX_SCORE]; MAX_POS],
     }
     println!("");
 
+    thread::sleep(time::Duration::from_secs(1));
+
 }
 
 
-fn move_pawns(player1_states: &mut [[u64; MAX_SCORE]; MAX_POS],
-    player2_states: &mut [[u64; MAX_SCORE]; MAX_POS], throw_map: &HashMap<usize, u64>) {
+fn move_pawns(player_states: &mut [[u64; MAX_SCORE]; MAX_POS], throw_map: &HashMap<usize, u64>) {
 
-    let mut new_player1_states : [[u64; MAX_SCORE]; MAX_POS] = [[0; MAX_SCORE]; MAX_POS];
-    let mut new_player2_states : [[u64; MAX_SCORE]; MAX_POS] = [[0; MAX_SCORE]; MAX_POS];
+    let mut new_player_states : [[u64; MAX_SCORE]; MAX_POS] = [[0; MAX_SCORE]; MAX_POS];
 
-    // Update player 1 states
-    for (posn, posn_scores) in player1_states.iter().enumerate() {
+    // Update player states
+    for (posn, posn_scores) in player_states.iter().enumerate() {
         for (score, score_count) in posn_scores.iter().enumerate() {
             // For each position, score count, apply all throws
             if *score_count == 0 {continue;}  // do nothing if no pawns in this position/ score
@@ -99,30 +145,13 @@ fn move_pawns(player1_states: &mut [[u64; MAX_SCORE]; MAX_POS],
                 let new_score = score + new_posn + 1;
                 let new_score_count = score_count * throw_count;
 
-                new_player1_states[new_posn][new_score] += new_score_count;
+                new_player_states[new_posn][new_score] += new_score_count;
             }
         }
     }
 
-    *player1_states = new_player1_states;
+    *player_states = new_player_states;
 
-    // Update player 2 states
-    for (posn, posn_scores) in player2_states.iter().enumerate() {
-        for (score, score_count) in posn_scores.iter().enumerate() {
-            // For each position, score count, apply all throws
-            if *score_count == 0 {continue;}  // do nothing if no pawns in this position/ score
-
-            for (throw_total, throw_count) in throw_map.iter() {
-                let new_posn = (posn + throw_total) % MAX_POS;
-                let new_score = score + new_posn + 1;
-                let new_score_count = score_count * throw_count;
-
-                new_player2_states[new_posn][new_score] += new_score_count;
-            }
-        }
-    }
-
-    *player2_states = new_player2_states;
 }
 
 
