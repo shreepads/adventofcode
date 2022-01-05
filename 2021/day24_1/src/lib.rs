f8f22ea10060f51334efd1da80ad86690abfd209
Shreepad S <shreepads@gmail.com>
2022-01-06 00:01:12 +0530

    Attempt to safecrack serial number

--- 2021/day24_1/src/lib.rs
+++ 2021/day24_1/src/lib.rs
@@ -28,19 +28,21 @@ pub fn calculate_max_serialno(file_path: &String) -> i64 {
         println!("*************************");
         //println!("ALU state: {:?} {:?}", alu.var_mins, alu.var_maxs);
         //println!("ALU state: {:?}", alu);
-        for (var, vals) in alu.var_values.iter() {
-            if vals.len() > 20 {
-                println!("{}: {} - {}", var, vals.iter().min().unwrap(), vals.iter().max().unwrap());
-            } else {
-                println!("{}: {:?}", var, vals);
-            }
-        }
+
         //thread::sleep(time::Duration::from_secs(1));
     }
 
     println!("\n\n\n\n\n");
     println!("*************************");
     //println!("ALU z state: {:?}", alu.vars.get(&"z".to_string()).unwrap());
+
+    for (var, vals) in alu.var_values.iter() {
+        if vals.len() > 20 {
+            println!("{}: {} - {}", var, vals.iter().min().unwrap(), vals.iter().max().unwrap());
+        } else {
+            println!("{}: {:?}", var, vals);
+        }
+    }
 
     0
 }
@@ -110,7 +112,7 @@ mod tests {
     #[test]
     fn test_prod() {
         let result =
-            calculate_max_serialno(&String::from("../resources/tests/day24-1-input-v1.txt"));
+            calculate_max_serialno(&String::from("../resources/tests/day24-1-input-v12.txt"));
         assert_eq!(result, 1);
     }
 
