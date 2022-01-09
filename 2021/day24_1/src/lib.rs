2e5289cb71f18c8602d608072b8272c3441d3d98
Shreepad S <shreepads@gmail.com>
2022-01-09 11:19:43 +0530

    Attempt to brute force with cracked values for specific digits

--- 2021/day24_1/src/lib.rs
+++ 2021/day24_1/src/lib.rs
@@ -1,15 +1,15 @@
 // Copyright (c) 2022 Shreepad Shukla
 // SPDX-License-Identifier: MIT
 
-mod alu;
-//mod alu2;
+//mod alu;
+mod alu2;
 
 use std::fs;
 use std::{thread, time};
 
 
-use alu::*;
-//use alu2::*;
+//use alu::*;
+use alu2::*;
 
 pub fn calculate_max_serialno(file_path: &String) -> i64 {
     println!("Loading data from file:{}", file_path);
@@ -19,29 +19,61 @@ pub fn calculate_max_serialno(file_path: &String) -> i64 {
         file_path
     ));
 
-    let mut alu = Alu::new();
+    'outer: for i1 in (1..=4).rev() {
+        println!("i1: {}", i1);
+        for i2 in (1..=9).rev() {
+            println!("i2: {}", i2);
+            for i3 in (1..=9).rev() {
+                println!("i3: {}", i3);
+                for i4 in (1..=9).rev() {
+                    println!("i4: {}", i4);
+                    for i5 in (8..=9).rev() {
+                        for i6 in (1..=2).rev() {
+                            for i7 in (1..=9).rev() {
+                                for i8 in (1..=7).rev() {
+                                    for i9 in (3..=9).rev() {
+                                        for i10 in (1..=4).rev() {
+                                            for i11 in (6..=9).rev() {
+                                                for i12 in (1..=9).rev() {
+                                                    for i13 in (9..=9).rev() {
+                                                        for i14 in (9..=9).rev() {
+                                                            let mut alu = Alu2::new();
+                                                            let input: [i64; 14] = [
+                                                                i1, i2, i3, i4, i5, i6, i7, i8, i9,
+                                                                i10, i11, i12, i13, i14,
+                                                            ];
 
-    for (i, line) in contents.lines().enumerate() {
-        alu.process_instruction(line.to_string());
-        println!("*************************");
-        println!("Line {}: {}", i, line);
-        println!("*************************");
-        //println!("ALU state: {:?} {:?}", alu.var_mins, alu.var_maxs);
-        //println!("ALU state: {:?}", alu);
-        for (var, vals) in alu.var_values.iter() {
-            if vals.len() > 26 {
-                println!("{}: {} - {}", var, vals.iter().min().unwrap(), vals.iter().max().unwrap());
-            } else {
-                println!("{}: {:?}", var, vals);
+                                                            for line in contents.lines() {
+                                                                alu.process_instruction(
+                                                                    line.to_string(),
+                                                                    input,
+                                                                );
+                                                            }
+
+                                                            if *alu.vars.get("z").unwrap() == 0 {
+                                                                println!(
+                                                                    "Found serial number: {:?}",
+                                                                    input
+                                                                );
+                                                                break 'outer;
+                                                            } else {
+                                                                //println!("Not serial number: {:?}", input);
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
-    
-        //thread::sleep(time::Duration::from_secs(1));
     }
 
-    println!("\n\n\n\n\n");
-    println!("*************************");
-    //println!("ALU z state: {:?}", alu.vars.get(&"z".to_string()).unwrap());
 
 
     0
