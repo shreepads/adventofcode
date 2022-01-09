a60176afc4f37e59d5dbc6b9f2aede7ce62ca952
Shreepad S <shreepads@gmail.com>
2022-01-09 16:48:46 +0530

    Setup ula module for baack calculation

--- 2021/day24_1/src/lib.rs
+++ 2021/day24_1/src/lib.rs
@@ -3,14 +3,18 @@
 
 mod alu;
 mod alu2;
+mod ula;
 
 use std::fs;
 use std::{thread, time};
-use rand::prelude::*;
+use std::collections::HashMap;
+use std::collections::HashSet;
 
+use rand::prelude::*;
 
 use alu::*;
 use alu2::*;
+use ula::Ula;
 
 pub fn calculate_max_serialno(file_path: &String) -> i64 {
     println!("Loading data from file:{}", file_path);
@@ -20,62 +24,24 @@ pub fn calculate_max_serialno(file_path: &String) -> i64 {
         file_path
     ));
 
-    let mut test_numbers = [[0i64; 14]; 10];
-    let mut rng = thread_rng();
-    
-
-    // Generate 10 random serial numbers to test both alus
-    for test_number in test_numbers.iter_mut() {
-        for digit in test_number.iter_mut() {
-            *digit = rng.gen_range(1..=9);
-        }
-    }
-
     // Setup alu
     let mut alu = Alu::new();
+    let mut alu_vals : Vec<HashMap<String, HashSet<i64>>> = Vec::new();
 
     for line in contents.lines() {
         alu.process_instruction(line.to_string());
+        alu_vals.push(alu.var_values.clone());
     }
 
-    for test_number in test_numbers {
-
-        println!("Test number: {:?}", test_number);
-        println!("*****************************");
-
-        println!("Alu:");
-        println!("--------");
-        println!("w calc: {}", alu.calculate_var("w".to_string(), test_number));
-        println!("x calc: {}", alu.calculate_var("x".to_string(), test_number));
-        println!("y calc: {}", alu.calculate_var("y".to_string(), test_number));
-
-        /*
-        println!("z min max: {} {}", alu.var_mins.get("z").unwrap(), alu.var_maxs.get("z").unwrap());
-        let min_val = alu.var_values.get("z").unwrap().iter().min().unwrap();
-        let max_val = alu.var_values.get("z").unwrap().iter().max().unwrap();
-        println!("z values range: {} - {}", min_val, max_val);
-        */
-        println!("z calc: {}", alu.calculate_var("z".to_string(), test_number));
-
-
-        println!("Alu v2");
-        println!("--------");
-        let mut alu2 = Alu2::new();
-
-        for line in contents.lines() {
-            alu2.process_instruction(
-                line.to_string(),
-                test_number,
-            );
-        }
-
-        println!("w calc: {}", alu2.vars.get("w").unwrap());
-        println!("x calc: {}", alu2.vars.get("x").unwrap());
-        println!("y calc: {}", alu2.vars.get("y").unwrap());
-        println!("z calc: {}\n", alu2.vars.get("z").unwrap());
-
+    // Setp ula
+    let mut ula = Ula::new();
+    let mut alu_vals_rev = alu_vals.iter().rev();
+    // skip the last state vals in alu_vals
+    alu_vals_rev.next();
+    for line in contents.lines().rev() {
+        ula.process_instruction(line.to_string(), alu_vals_rev.next().unwrap());
+        println!("Ula state: {:?}", ula);
     }
-
 
     0
 }
---
+++ 2021/day24_1/src/ula.rs
@@ -0,0 +1,150 @@
+// Copyright (c) 2022 Shreepad Shukla
+// SPDX-License-Identifier: MIT
+
+use std::collections::HashMap;
+use std::collections::HashSet;
+
+use crate::alu::Value;
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Operation {
+    Add,
+    Mul,
+    Div,
+    Mod,
+    Eql,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum UlaValue {
+    Unknown(HashSet<i64>),
+    Known(i64),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Ula {
+    pub vars: HashMap<String, UlaValue>,
+    current_input: usize,
+}
+
+impl Ula {
+    pub fn new() -> Ula {
+        use crate::ula::UlaValue::{Known, Unknown};
+
+        Ula {
+            vars: HashMap::from([
+                ("w".to_string(), Unknown(HashSet::new())),
+                ("x".to_string(), Unknown(HashSet::new())),
+                ("y".to_string(), Unknown(HashSet::new())),
+                ("z".to_string(), Known(0)),
+            ]),
+            current_input: 0,
+        }
+    }
+
+    pub fn process_instruction(&mut self, instr: String, alu_vals: &HashMap<String, HashSet<i64>>) {
+
+        println!("Processing instr {} with vals:", instr);
+        for (var, vals) in alu_vals.iter() {
+            print!("{}: ", var);
+            if vals.len() > 26 {
+                println!("{} - {}", vals.iter().min().unwrap(), vals.iter().max().unwrap());
+            } else {
+                println!("{:?}", vals.iter());
+            }
+        }
+        
+        let mut parts = instr.split(' ');
+
+        let op = parts.next().unwrap();
+
+        let left = parts.next().unwrap();
+        if left.len() > 1 || !"wxyz".contains(left) {
+            println!("Invalid left in instr: {}", instr);
+        }
+
+        match op {
+            //"inp" => self.process_inp(left),
+            "add" => {
+                let right = parts.next().unwrap();
+                self.process_add(left, right, self.right_values(right, alu_vals), alu_vals);
+            },
+            _ => {
+                println!("Invalid instruction: {}", instr);
+            },
+            /*
+            "mul" => {
+                let right = parts.next().unwrap();
+                self.process_mul(left, self.right_val(right),
+                    self.right_min(right), self.right_max(right), self.right_values(right));
+            }
+            "div" => {
+                let right = parts.next().unwrap();
+                self.process_div(left, self.right_val(right),
+                    self.right_min(right), self.right_max(right), self.right_values(right));
+            }
+            "mod" => {
+                let right = parts.next().unwrap();
+                self.process_mod(left, self.right_val(right),
+                    self.right_min(right), self.right_max(right), self.right_values(right));
+            }
+            "eql" => {
+                let right = parts.next().unwrap();
+                self.process_eql(left, self.right_val(right),
+                    self.right_min(right), self.right_max(right), self.right_values(right));
+            }
+            _ => {
+                println!("Invalid instruction: {}", instr);
+            }*/
+        }
+    }
+
+
+    fn right_values(&self, right: &str, alu_vals: &HashMap<String, HashSet<i64>>) -> HashSet<i64> {
+
+        // figure out if right is a register or value
+        match right.parse::<i64>() {
+            Ok(num) => HashSet::from([num]),
+            Err(_) => alu_vals.get(right).unwrap().clone(),
+        }
+    }
+
+
+    fn process_add(&mut self, left: &str, right: &str, right_values: HashSet<i64>, alu_vals: &HashMap<String, HashSet<i64>>) {
+
+        use crate::ula::UlaValue::*;
+
+        let left_values: HashSet<i64> = alu_vals.get(left).unwrap().clone();
+
+        let left_val: UlaValue = self.vars.get(left).unwrap().clone();
+        let right_val: UlaValue = self.vars.get(right).unwrap().clone();
+
+        // If left value is known, check left/ right values for possible solutions
+        let mut new_left_values : HashSet<i64> = HashSet::new();
+        let mut new_right_values : HashSet<i64> = HashSet::new();
+
+        if let Known(left_v) = left_val {
+            for left_value in left_values.iter() {
+                for right_value in right_values.iter() {
+                    if left_value + right_value == left_v {
+                        new_left_values.insert(*left_value);
+                        new_right_values.insert(*right_value);
+                    }
+                }
+            }
+        }
+
+        if new_left_values.len() == 1 {
+            self.vars.insert(left.to_string(), Known(*new_left_values.iter().nth(0).unwrap()));
+        } else {
+            self.vars.insert(left.to_string(), Unknown(HashSet::new()));
+        }
+
+        if new_right_values.len() == 1 {
+            self.vars.insert(right.to_string(), Known(*new_right_values.iter().nth(0).unwrap()));
+        }
+
+
+    }
+
+}
