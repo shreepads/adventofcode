ab9370e90ffbeb4a790ff9c19a3cd128b0cfdcf9
Shreepad S <shreepads@gmail.com>
2022-12-08 20:04:07 +0530

    Setup for 2022

---
+++ 2022/day1/src/lib.rs
@@ -0,0 +1,17 @@
+// Copyright (c) 2022 Shreepad Shukla
+// SPDX-License-Identifier: MIT
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
---
+++ 2022/y2022/src/main.rs
@@ -0,0 +1,9 @@
+// Copyright (c) 2022 Shreepad Shukla
+// SPDX-License-Identifier: MIT
+
+fn main() {
+    println!(
+        "Day 1, #1: {}",
+        day1::add(2, 3)
+    );
+}
