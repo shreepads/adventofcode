26efc829876531cf7bc6ee549066c28a63dade2f
Shreepad S <shreepads@gmail.com>
2022-12-10 13:18:49 +0530

    Solve Day 1

--- 2022/day1/src/lib.rs
+++ 2022/day1/src/lib.rs
@@ -1,17 +1,51 @@
 // Copyright (c) 2022 Shreepad Shukla
 // SPDX-License-Identifier: MIT
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::fs;
+
+pub fn most_cals_carried(file_path: &String) -> u64 {
+
+    let list_of_lists_of_number = load_list_of_lists_of_unumbers(file_path);
+
+    list_of_lists_of_number.iter()
+        .map(|list_of_unumbers| list_of_unumbers.iter().sum())
+        .max()
+        .unwrap()
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn top3_total_cals_carried(file_path: &String) -> u64 {
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    let list_of_lists_of_number = load_list_of_lists_of_unumbers(file_path);
+
+    let mut list_of_total_cals = list_of_lists_of_number.iter()
+        .map(|list_of_unumbers| list_of_unumbers.iter().sum())
+        .collect::<Vec<u64>>();
+
+    list_of_total_cals.sort_unstable();
+    list_of_total_cals.reverse();
+
+    list_of_total_cals
+        .split_at(3).0
+        .iter()
+        .sum()
+
+}
+
+fn load_list_of_lists_of_unumbers(file_path: &String) -> Vec<Vec<u64>> {
+    
+    let mut list_of_lists_of_unumbers: Vec<Vec<u64>> = Vec::new();
+
+    println!("Loading data from file:{}", file_path);
+    let file_contents = fs::read_to_string(file_path).expect("Something went wrong reading the file");
+
+    for list_of_string_unumbers in file_contents.split("\n\n") {
+        let list_of_unumbers: Vec<u64> = list_of_string_unumbers.lines()
+            .map(|string_unumber| string_unumber.parse::<u64>().unwrap())
+            .collect();
+
+        list_of_lists_of_unumbers.push(list_of_unumbers);
     }
+
+    list_of_lists_of_unumbers
 }
+
--- 2022/y2022/src/main.rs
+++ 2022/y2022/src/main.rs
@@ -4,6 +4,12 @@
 fn main() {
     println!(
         "Day 1, #1: {}",
-        day1::add(2, 3)
+        day1::most_cals_carried(&String::from("resources/input/day01-1.txt"))
     );
+
+    println!(
+        "Day 1, #2: {}",
+        day1::top3_total_cals_carried(&String::from("resources/input/day01-1.txt"))
+    );
+
 }
